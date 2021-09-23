7890f90c138e45fb616984e04fd62e3b798ff82d
Petzi <daboarderpjb@gmail.com>
2021-09-23 18:18:24 +0200

    expansion Lists

--- src/components/DevTest.vue
+++ src/components/DevTest.vue
@@ -4,78 +4,80 @@
     <v-row>
       <v-col>
         <v-card>
-          <v-card-title> Plane </v-card-title>
-          <v-card-text>
-            <v-list>
-              <v-list-item v-for="(p, i) in planes" :key="i">
-                <v-list-item-avatar tile height="80" width="100">
+          <v-card-title> Flugzeuge </v-card-title>
+
+          <v-list>
+            <v-list-group v-for="p in planes" :key="p.name" multiple>
+              <template v-slot:activator>
+                <v-list-item-avatar tile height="80" width="200">
                   <v-img
                     :alt="p.image"
-                    contain
+                    cover
                     :src="require('@/assets/' + p.image + '_00000.jpg')"
                   ></v-img>
                 </v-list-item-avatar>
                 <v-list-item-content>
-                  <v-form>
-                    <v-row>
-                      <v-col>
-                        <v-icon :color="checkIfNotEmpty(p.name).color">{{
-                          checkIfNotEmpty(p.name).icon
-                        }}</v-icon>
-                        Name: {{ p.name }}
-                      </v-col>
-                      <v-col>
-                        <v-icon :color="checkIfNotEmpty(p.type).color">{{
-                          checkIfNotEmpty(p.type).icon
-                        }}</v-icon>
-                        Typ: {{ p.type }}
-                      </v-col>
-                      <v-col>
-                        <v-icon :color="checkIfNotEmpty(p.bauweise).color">{{
-                          checkIfNotEmpty(p.bauweise).icon
-                        }}</v-icon>
-                        Bauweise: {{ p.bauweise }}
-                      </v-col>
-                      <v-col>
-                        <v-icon :color="checkIfNotEmpty(p.gewicht).color">{{
-                          checkIfNotEmpty(p.gewicht).icon
-                        }}</v-icon>
-                        Gewicht: {{ p.gewicht }}
-                      </v-col>
-                      <v-col>
-                        <v-icon :color="checkIfNotEmpty(p.spannweite).color">{{
-                          checkIfNotEmpty(p.spannweite).icon
-                        }}</v-icon>
-                        Spannweite: {{ p.spannweite }}
-                      </v-col>
-                      <v-col>
-                        <v-icon :color="checkIfNotEmpty(p.faktor).color">{{
-                          checkIfNotEmpty(p.faktor).icon
-                        }}</v-icon>
-                        Faktor: {{ p.faktor }}
-                      </v-col>
-                      <v-col>
-                        <v-icon :color="checkIfNotEmpty(p.sender).color">{{
-                          checkIfNotEmpty(p.sender).icon
-                        }}</v-icon>
-                        Sender: {{ p.sender }}
-                      </v-col>
-                      <v-col>
-                        <v-icon
-                          :color="checkIfNotEmpty(p.beschreibung).color"
-                          >{{ checkIfNotEmpty(p.beschreibung).icon }}</v-icon
-                        >
-                        Beschreibung: {{ p.beschreibung }}
-                      </v-col>
-                      <v-col>
-                        <v-btn @click="dialogOpen(p)">Bearbeiten</v-btn>
-                      </v-col>
-                    </v-row>
-                  </v-form>
+                  <v-row>
+                    <v-col cols="12" sm="6" md="3">
+                      <v-icon :color="checkIfNotEmpty(p.name).color">{{
+                        checkIfNotEmpty(p.name).icon
+                      }}</v-icon>
+                      <span class="headline">{{ p.name }}</span>
+                    </v-col>
+                    <v-col cols="12" sm="6" md="3">
+                      <v-icon :color="checkIfNotEmpty(p.type).color">{{
+                        checkIfNotEmpty(p.type).icon
+                      }}</v-icon>
+                      Typ: {{ p.type }}
+                    </v-col>
+                    <v-col cols="12" sm="6" md="3">
+                      <v-icon :color="checkIfNotEmpty(p.bauweise).color">{{
+                        checkIfNotEmpty(p.bauweise).icon
+                      }}</v-icon>
+                      Bauweise: {{ p.bauweise }}
+                    </v-col>
+                    <v-col cols="12" sm="6" md="3">
+                      <v-icon :color="checkIfNotEmpty(p.gewicht).color">{{
+                        checkIfNotEmpty(p.gewicht).icon
+                      }}</v-icon>
+                      Gewicht: {{ p.gewicht }}
+                    </v-col>
+                    <v-col cols="12" sm="6" md="3">
+                      <v-icon :color="checkIfNotEmpty(p.spannweite).color">{{
+                        checkIfNotEmpty(p.spannweite).icon
+                      }}</v-icon>
+                      Spannweite: {{ p.spannweite }}
+                    </v-col>
+                    <v-col cols="12" sm="6" md="3">
+                      <v-icon :color="checkIfNotEmpty(p.faktor).color">{{
+                        checkIfNotEmpty(p.faktor).icon
+                      }}</v-icon>
+                      Faktor: {{ p.faktor }}
+                    </v-col>
+                    <v-col cols="12" sm="6" md="3">
+                      <v-icon :color="checkIfNotEmpty(p.sender).color">{{
+                        checkIfNotEmpty(p.sender).icon
+                      }}</v-icon>
+                      Sender: {{ p.sender }}
+                    </v-col>
+                  </v-row>
+                </v-list-item-content>
+              </template>
+
+              <v-list-item>
+                <v-list-item-content>
+                  <v-card flat color="#e0e0e0">
+                    <v-card-title>{{ p.name }}</v-card-title
+                    ><v-card-text>{{ p.beschreibung }}</v-card-text>
+                    <v-card-actions>
+                      <v-btn @click="dialogOpen(p)">Bearbeiten</v-btn>
+                    </v-card-actions></v-card
+                  >
                 </v-list-item-content>
               </v-list-item>
-            </v-list>
-          </v-card-text>
+              <v-divider></v-divider>
+            </v-list-group>
+          </v-list>
         </v-card>
         Editieren: {{ editPlane }}
         <PlaneDialog v-model="dialog" :plane="editPlane" />
@@ -104,6 +106,7 @@ export default class DevTest extends Vue {
   img = [{ plane: "", img: require("@/assets/logo.png") }];
   editPlane: Plane | null = null;
   dialog = false;
+  active = false;
 
   dialogOpen(p: Plane): void {
     this.editPlane = p;
@@ -133,6 +136,11 @@ export default class DevTest extends Vue {
         .withSpannweite(p.spannweite)
         .withImage(p.image);
 
+      if (p.beschreibung) {
+        plane.withBeschreibung(p.beschreibung);
+      } else {
+        plane.withBeschreibung("");
+      }
       if (typeof p.faktor === "string") {
         const newfaktor = p.faktor.replace(",", ".");
         plane.withFaktor(parseFloat(newfaktor));
@@ -167,4 +175,8 @@ export default class DevTest extends Vue {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.inlay {
+  background-color: lightcyan;
+}
+</style>
